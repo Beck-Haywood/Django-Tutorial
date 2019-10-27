5ed6fbab0fe99cceb0fa7869b2f64ae4f515bb06
Beck-Haywood <beck.haywood@students.makeschool.com>
2019-10-26 17:37:47 -0700

    Updated models.py and settings

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,13 +4,19 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return '%s %s' % (self.question_text, self.pub_date)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-class Topping(models.Model):
+    def __str__(self):
+        return '%s %s %s' % (self.question, self.choice_text, self.votes)
+
+'''class Topping(models.Model):
     name = models.CharField(max_length=12)
 
     def __str__(self):
@@ -27,3 +33,4 @@ class Pizza(models.Model):
     pizza = models.CharField(max_length=12)
     def __str__(self):
         return self.toppings
+        '''
