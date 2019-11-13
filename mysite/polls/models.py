478fc305070d21dbd0c015fe67b7fc49787daac6
Beck-Haywood <beck.haywood@students.makeschool.com>
2019-11-13 15:17:37 -0800

    I went through page 4 of tutorial, having problems with question and choice data

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -15,25 +15,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return '%s %s %s' % (self.question, self.choice_text, self.votes)
-
-# myclub_project\events\models.py
-
-
-'''class Topping(models.Model):
-    name = models.CharField(max_length=12)
-
-    def __str__(self):
-        return self.name
-
-
-
-
-class Pizza(models.Model):
-    toppings = models.ManyToManyField(Topping)
-    cost = models.IntegerField(default=8.00)
-    amount_of_pizzas = models.IntegerField(default=1)
-    pizza_size = models.IntegerField(default=8)
-    pizza = models.CharField(max_length=12)
-    def __str__(self):
-        return self.toppings
-        '''
